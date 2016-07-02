4f5effe33ef61657ba0942f1e5b175eb55f62725
Victor Grigoriu <vgrigoriu@gmail.com>
2016-07-02 18:00:34 +0300

    Move state enumeratio to separate file

--- CapNetStandard/SpaceDelimitedElementsParser.cs
+++ CapNetStandard/SpaceDelimitedElementsParser.cs
@@ -30,7 +30,7 @@ namespace CAPNet
         /// <returns>Elements in a IEnumerable&lt;string></returns>
         public IEnumerable<string> GetElements()
         {
-            var currentState = States.BETWEEN_ELEMENTS;
+            var currentState = SpaceDelimitedElementsParserState.BETWEEN_ELEMENTS;
 
             for (currentPosition = 0; currentPosition < representation.Length; currentPosition++)
             {
@@ -53,116 +53,18 @@ namespace CAPNet
             return elements;
         }
 
-        private void AddCurrentElementToList()
+        internal void AddCurrentElementToList()
         {
             elements.Add(partialElement.ToString());
             partialElement.Clear();
         }
 
-        private void AddToCurrentElement(char currentChar)
+        internal void AddToCurrentElement(char currentChar)
         {
             partialElement.Append(currentChar);
             if (currentPosition == representation.Length - 1)
             {
                 elements.Add(partialElement.ToString());
-            }
-        }
-
-        private abstract class States : Enumeration<States>
-        {
-#pragma warning disable SA1310 // Field names must not contain underscore
-            public static readonly States BETWEEN_ELEMENTS = new BetweenElements();
-            public static readonly States IN_SPACE_CONTAINING_ELEMENTS = new InSpaceContainingElement();
-            public static readonly States IN_ELEMENTS_WITH_NO_SPACE = new InElementWithNoSpace();
-#pragma warning restore SA1310 // Field names must not contain underscore
-
-            private States(int value, string displayName)
-                : base(value, displayName)
-            {
-            }
-
-            public abstract States OnSpace(SpaceDelimitedElementsParser parser, char currentChar);
-
-            public abstract States OnQuote(SpaceDelimitedElementsParser parser);
-
-            public abstract States OnOtherCharacter(SpaceDelimitedElementsParser parser, char currentChar);
-
-            private class BetweenElements : States
-            {
-                public BetweenElements()
-                    : base(0, "Between elements")
-                {
-                }
-
-                public override States OnOtherCharacter(SpaceDelimitedElementsParser parser, char currentChar)
-                {
-                    parser.AddToCurrentElement(currentChar);
-
-                    return IN_ELEMENTS_WITH_NO_SPACE;
-                }
-
-                public override States OnQuote(SpaceDelimitedElementsParser parser)
-                {
-                    return IN_SPACE_CONTAINING_ELEMENTS;
-                }
-
-                public override States OnSpace(SpaceDelimitedElementsParser parser, char currentChar)
-                {
-                    // we're still between elements
-                    return this;
-                }
-            }
-
-            private class InSpaceContainingElement : States
-            {
-                public InSpaceContainingElement()
-                    : base(1, "In space containing element")
-                {
-                }
-
-                public override States OnOtherCharacter(SpaceDelimitedElementsParser parser, char currentChar)
-                {
-                    parser.AddToCurrentElement(currentChar);
-                    return this;
-                }
-
-                public override States OnQuote(SpaceDelimitedElementsParser parser)
-                {
-                    parser.AddCurrentElementToList();
-                    return BETWEEN_ELEMENTS;
-                }
-
-                public override States OnSpace(SpaceDelimitedElementsParser parser, char currentChar)
-                {
-                    parser.AddToCurrentElement(currentChar);
-                    return this;
-                }
-            }
-
-            private class InElementWithNoSpace : States
-            {
-                public InElementWithNoSpace()
-                    : base(2, "In element with no space")
-                {
-                }
-
-                public override States OnOtherCharacter(SpaceDelimitedElementsParser parser, char currentChar)
-                {
-                    parser.AddToCurrentElement(currentChar);
-                    return this;
-                }
-
-                public override States OnQuote(SpaceDelimitedElementsParser parser)
-                {
-                    // this is an opening quote without a preceding space
-                    return this;
-                }
-
-                public override States OnSpace(SpaceDelimitedElementsParser parser, char currentChar)
-                {
-                    parser.AddCurrentElementToList();
-                    return BETWEEN_ELEMENTS;
-                }
             }
         }
     }
---
+++ CapNetStandard/SpaceDelimitedElementsParserState.cs
@@ -0,0 +1,100 @@
+﻿namespace CAPNet
+{
+    internal abstract class SpaceDelimitedElementsParserState : Enumeration<SpaceDelimitedElementsParserState>
+    {
+#pragma warning disable SA1310 // Field names must not contain underscore
+        public static readonly SpaceDelimitedElementsParserState BETWEEN_ELEMENTS = new BetweenElements();
+        public static readonly SpaceDelimitedElementsParserState IN_SPACE_CONTAINING_ELEMENTS = new InSpaceContainingElement();
+        public static readonly SpaceDelimitedElementsParserState IN_ELEMENTS_WITH_NO_SPACE = new InElementWithNoSpace();
+#pragma warning restore SA1310 // Field names must not contain underscore
+
+        private SpaceDelimitedElementsParserState(int value, string displayName)
+            : base(value, displayName)
+        {
+        }
+
+        public abstract SpaceDelimitedElementsParserState OnSpace(SpaceDelimitedElementsParser parser, char currentChar);
+
+        public abstract SpaceDelimitedElementsParserState OnQuote(SpaceDelimitedElementsParser parser);
+
+        public abstract SpaceDelimitedElementsParserState OnOtherCharacter(SpaceDelimitedElementsParser parser, char currentChar);
+
+        private class BetweenElements : SpaceDelimitedElementsParserState
+        {
+            public BetweenElements()
+                : base(0, "Between elements")
+            {
+            }
+
+            public override SpaceDelimitedElementsParserState OnOtherCharacter(SpaceDelimitedElementsParser parser, char currentChar)
+            {
+                parser.AddToCurrentElement(currentChar);
+
+                return IN_ELEMENTS_WITH_NO_SPACE;
+            }
+
+            public override SpaceDelimitedElementsParserState OnQuote(SpaceDelimitedElementsParser parser)
+            {
+                return IN_SPACE_CONTAINING_ELEMENTS;
+            }
+
+            public override SpaceDelimitedElementsParserState OnSpace(SpaceDelimitedElementsParser parser, char currentChar)
+            {
+                // we're still between elements
+                return this;
+            }
+        }
+
+        private class InSpaceContainingElement : SpaceDelimitedElementsParserState
+        {
+            public InSpaceContainingElement()
+                : base(1, "In space containing element")
+            {
+            }
+
+            public override SpaceDelimitedElementsParserState OnOtherCharacter(SpaceDelimitedElementsParser parser, char currentChar)
+            {
+                parser.AddToCurrentElement(currentChar);
+                return this;
+            }
+
+            public override SpaceDelimitedElementsParserState OnQuote(SpaceDelimitedElementsParser parser)
+            {
+                parser.AddCurrentElementToList();
+                return BETWEEN_ELEMENTS;
+            }
+
+            public override SpaceDelimitedElementsParserState OnSpace(SpaceDelimitedElementsParser parser, char currentChar)
+            {
+                parser.AddToCurrentElement(currentChar);
+                return this;
+            }
+        }
+
+        private class InElementWithNoSpace : SpaceDelimitedElementsParserState
+        {
+            public InElementWithNoSpace()
+                : base(2, "In element with no space")
+            {
+            }
+
+            public override SpaceDelimitedElementsParserState OnOtherCharacter(SpaceDelimitedElementsParser parser, char currentChar)
+            {
+                parser.AddToCurrentElement(currentChar);
+                return this;
+            }
+
+            public override SpaceDelimitedElementsParserState OnQuote(SpaceDelimitedElementsParser parser)
+            {
+                // this is an opening quote without a preceding space
+                return this;
+            }
+
+            public override SpaceDelimitedElementsParserState OnSpace(SpaceDelimitedElementsParser parser, char currentChar)
+            {
+                parser.AddCurrentElementToList();
+                return BETWEEN_ELEMENTS;
+            }
+        }
+    }
+}
